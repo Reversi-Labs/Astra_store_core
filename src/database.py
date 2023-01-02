7508736c3d6a645dd2b08705bd196832d0c1ef0c
Egor Bakay <egor_bakay@inbox.ru>
2023-01-02 18:45:00 +0300

    some update structure and integraded in main

--- src/database.py
+++ src/database.py
@@ -27,7 +27,7 @@ class BD_node:
         self.remove = remove
 
 
-class Excel:
+class BD:
 
     def __init__(self,path=str(os.getcwd())+"/BD.xls"):
         self.path = path
@@ -36,14 +36,19 @@ class Excel:
         self.setup = {}
         self.install = {}
         self.error = {}
+        self.update = {}
 
-    def read(self):
-        # Open a workbook 
-        workbook = xlrd.open_workbook(self.path)
-        # read INSTALL instructions:
-        for i in range(3):
-            # Load a specific sheet by index 
-            worksheet = workbook.sheet_by_index(i)
+    def print_BD(self):
+        print(self.setup)
+        print(self.install)
+        print(self.error)
+
+    def read_BD(self):
+
+        def read_one_list(name_list): # workbook,
+            # Load a specific sheet by name
+            try: worksheet = workbook.sheet_by_name(name_list) # .sheet_by_index(i)
+            except xlrd.biffh.XLRDError: return [] # end lists
             # read all list
             read = []
             a = 0
@@ -59,51 +64,45 @@ class Excel:
                     del read[a]
                     break
                 a+=1
-            # unwrap
-            if   i==0: self.setup   = self.unwrap_install(read)
-            elif i==1: self.install = self.unwrap_install(read)
-            else:      self.error   = self.unwrap_install(read,error_mode=True)
-            # end
-        #return self.database
+            return read
 
-    def unwrap_install(self,data,error_mode=False):
-        answer = {}
-        for y in range (1,len(data)):
-            if data[y][0]!='': 
-                #print(data[y])
-                name = data[y][0]
-                node = 0
-                if error_mode: node = [data[y][1]]
-                else: node = BD_node([data[y][1]],[data[y][2]])
-                try:
-                    y+=1
-                    while data[y][0]=='':
-                        #print(" ",data[y]) 
-                        if error_mode:
-                            if data[y][1]!='': node.append(data[y][1])
-                        else:
-                            if data[y][1]!='': node.install.append(data[y][1])
-                            if data[y][2]!='': node.remove.append(data[y][2])
+        def unwrap_data(data,error_mode=False):
+            answer = {}
+            for y in range (1,len(data)):
+                if data[y][0]!='': 
+                    #print(data[y])
+                    name = data[y][0]
+                    node = 0
+                    if error_mode: node = [data[y][1]]
+                    else: node = BD_node([data[y][1]],[data[y][2]])
+                    try:
                         y+=1
-                except IndexError: pass
-                answer[name] = node
-        return answer
+                        while data[y][0]=='':
+                            #print(" ",data[y]) 
+                            if error_mode:
+                                if data[y][1]!='': node.append(data[y][1])
+                            else:
+                                if data[y][1]!='': node.install.append(data[y][1])
+                                if data[y][2]!='': node.remove.append(data[y][2])
+                            y+=1
+                    except IndexError: pass
+                    answer[name] = node
+            return answer
+
+        def read_list(name_list,error_mode=False):
+            return unwrap_data(read_one_list(name_list),error_mode)
+
+        # Open a workbook 
+        workbook = xlrd.open_workbook(self.path)
+        # read:
+        self.setup   = read_list("setup")
+        self.install = read_list("install")
+        self.error   = read_list("error",error_mode=True)
+        self.update  = read_list("update",error_mode=True)
 
 if __name__=="__main__":
 
-    excel = Excel()
-
-    data = excel.read()
-    #print(excel.get())
-    # for a in data:
-    #     print(a)
-
-    # hashmap = {'jack': 4098, 'sape': 4139}
-    # print(hashmap)
-    # hashmap['guido'] = 4127
-    # print(hashmap)
-
+    excel = BD()
+    data = excel.read_BD()
     print("=====================")
-    print(excel.setup)
-    print(excel.install)
-    print(excel.error)
+    excel.print_BD()
