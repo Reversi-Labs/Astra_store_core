d9a2587511b4d4de1d61ed44424feb04877645de
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2023-01-07 13:09:10 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+echo Install libs...
+sudo apt update
+sudo apt upgrade
+sudo apt install python3-pip 
+sudo python3 -m pip install xlrd==1.2.0
+sudo python3 -m pip install requests
+echo Install libs - OK
+
+# echo Load project...(alpha)...
+# wget https://github.com/Reversi-Labs/Astra_store_core/archive/refs/heads/alpha.zip
+# sudo apt install unzip
+# unzip Astra_store_core-alpha.zip
+# echo Load project - OK
+
+echo Enjoy work with Astra_store_installer :)
+
+read -s -n 1
+# SETTINGS=`stty -g`
+# stty -echo
+# read -n 1
+# stty $SETTINGS
